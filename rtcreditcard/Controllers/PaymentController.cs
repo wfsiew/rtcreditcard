33196c9b7cd9a0c2024b143e1573db1407c99ed6
wfsiew <siewwingfei@hotmail.com>
2015-06-17 13:45:11 +0800

    a

--- rtcreditcard/Controllers/PaymentController.cs
+++ rtcreditcard/Controllers/PaymentController.cs
@@ -24,19 +24,6 @@ namespace rtcreditcard.Controllers
 
         public ActionResult Result()
         {
-            ViewBag.TRANSACTION_ID = Request.Form["TRANSACTION_ID"];
-            ViewBag.TXN_STATUS = Request.Form["TXN_STATUS"];
-            ViewBag.TXN_SIGNATURE = Request.Form["TXN_SIGNATURE"];
-            ViewBag.AUTH_ID = Request.Form["AUTH_ID"];
-            ViewBag.TRAN_DATE = Request.Form["TRAN_DATE"];
-            ViewBag.AUTH_DATE = Request.Form["AUTH_DATE"];
-            ViewBag.RESPONSE_CODE = Request.Form["RESPONSE_CODE"];
-            ViewBag.RESPONSE_DESC = Request.Form["RESPONSE_DESC"];
-            ViewBag.MERCHANT_TRANID = Request.Form["MERCHANT_TRANID"];
-            ViewBag.CUSTOMER_ID = Request.Form["CUSTOMER_ID"];
-            ViewBag.FR_LEVEL = Request.Form["FR_LEVEL"];
-            ViewBag.FR_SCORE = Request.Form["FR_SCORE"];
-
             var keys = Request.Form.Keys;
             Dictionary<string, string> d = new Dictionary<string, string>();
             foreach (string k in keys)
@@ -53,7 +40,7 @@ namespace rtcreditcard.Controllers
             StringBuilder sb = new StringBuilder("M>)\"&nR7")
             .Append("02700701127375000697")
             .Append(txnid)
-            .Append("0.01");
+            .Append("500.65");
             byte[] b = Encoding.UTF8.GetBytes(sb.ToString().ToCharArray());
             SHA512Managed x = new SHA512Managed();
             byte[] r = x.ComputeHash(b);
